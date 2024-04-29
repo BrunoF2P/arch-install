113b06a49c9ddeba1cd87db6b7466e66c6819d6d
Bruno Bispo <brunobispof2p@gmail.com>
2024-04-29 01:02:30 -0300

    update

--- base-uefi.sh
+++ base-uefi.sh
@@ -2,10 +2,9 @@
 
 ln -sf /usr/share/zoneinfo/America/Bahia /etc/localtime
 hwclock --systohc
-sed -i '178s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=br_abnt2" >> /etc/vconsole.conf
+echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
 echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
@@ -17,10 +16,10 @@ echo root:password | chpasswd
 
 pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
-# pacman -S --noconfirm xf86-video-amdgpu
+pacman -S xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
--- gnome.sh
+++ gnome.sh
@@ -2,8 +2,6 @@
 
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
-
-sudo reflector -c Brazilian -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Sy
 
 sudo firewall-cmd --add-port=1025-65535/tcp --permanent
@@ -12,7 +10,7 @@ sudo firewall-cmd --reload
 # sudo virsh net-autostart default
 
 
-sudo pacman -S --noconfirm xorg gdm gnome gnome-extra firefox gnome-tweaks dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+sudo pacman -S --noconfirm xorg gdm gnome gnome-extra firefox gnome-tweaks ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
 
 # sudo flatpak install -y spotify
 # sudo flatpak install -y kdenlive
